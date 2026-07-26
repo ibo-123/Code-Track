9b96027ea35c3672c27c30d96fcdb4bf176898cf
Ibrahim Kedir <ibrahimkediramdela2211@gmail.com>
2026-07-26 19:47:14 +0300

    -y

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
+console.log(import.meta.env.VITE_API_URL);
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
 });
-console.log(import.meta.env.VITE_API_URL);
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
 
