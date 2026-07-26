7a4279e4040ccd6638f985c9837adba8b6478a71
Ibrahim Kedir <ibrahimkediramdela2211@gmail.com>
2026-07-26 19:30:35 +0300

    -y

--- server/src/app.js
+++ server/src/app.js
@@ -13,7 +13,7 @@ dotenv.config();
 const app = express();
 app.use(
   cors({
-    origin: "https://code-track-nine.vercel.app/",
+    origin: "https://code-track-nine.vercel.app",
     credentials: true,
   })
 );
