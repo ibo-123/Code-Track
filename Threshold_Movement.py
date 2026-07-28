92b770c0aecdc6151edf571c37afc2eb86c43a02
Ibrahim Kedir <ibrahimkediramdela2211@gmail.com>
2026-07-28 08:50:04 +0300

    -y

--- Threshold_Movement.py
+++ Threshold_Movement.py
@@ -1,24 +1,11 @@
 for _ in range(int(input())):
     n = int(input())
     a = list(map(int, input().split()))
-
-    INF = 10 ** 18
-
-    def check(start_right):
-        low = -INF      # values that must be > k
-        high = INF      # values that must be < k
-
-        for i in range(n):
-            right = (i % 2 == 0) if start_right else (i % 2 == 1)
-
-            if right:
-                low = max(low, a[i])
-            else:
-                high = min(high, a[i])
-
-        return low + 1 <= high - 1
-
-    if check(True) or check(False):
-        print("YES")
-    else:
-        print("NO")
+    l = 0
+    r = float("inf")
+    for i in range(n+1):
+        if i % 2 == 1:
+           r = min(r,a[i-1])
+        else:
+            l = max(l,a[i-1])
+    print("YES" if (n %2 == 0) and (l+2 <= r) else "NO")
