738bd09c005c82c9079b4d9392528fca542326c2
Ibrahim Kedir <ibrahimkediramdela2211@gmail.com>
2026-08-02 11:51:16 +0300

    debuging verification email

--- server/src/services/email.service.js
+++ server/src/services/email.service.js
@@ -6,12 +6,15 @@ const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
   port: 587,
   secure: false,
-  requireTLS: true,
 
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
+
+  connectionTimeout: 10000,
+  greetingTimeout: 10000,
+  socketTimeout: 10000,
 
   family: 4,
 });
